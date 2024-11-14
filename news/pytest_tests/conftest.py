79408b7c6471154f7ddd4447b5f151961ee5069f
Геннадий <gennadiusisus@gmail.com wgit config --global user.email gennadiusisus@gmail.com>
2024-11-14 19:57:52 +0700

    test_comment_order

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 # Импортируем класс клиента.
 from django.test.client import Client
 from django.utils import timezone
+from datetime import datetime, timedelta
 # Импортируем модель заметки, чтобы создать экземпляр.
 from news.models import Comment, News
 
@@ -56,3 +57,39 @@ def comment(author, new):
     )
     #news=cls.news, author=cls.author, text=f'Tекст {index}',
     return com
+
+@pytest.fixture
+def many_news(author):
+    today = datetime.today()
+    all_news = [        
+        News(
+            title=f'Новость {index}',
+            text=f'Просто текст {index}',
+            # Для каждой новости уменьшаем дату на index дней от today,
+            # где index - счётчик цикла.
+            date=today - timedelta(days=index)
+        )
+        for index in range(11)
+        ]
+    News.objects.bulk_create(all_news)
+    return all_news
+
+
+
+@pytest.fixture
+def many_comments(author, new):
+    today = datetime.today()
+    all_comment = [
+        Comment(
+            news=new,
+            author=author,
+            text=f'Текст {index}',
+            created=today - timedelta(days=index),
+        )
+        for index in range(11)
+        ]
+    Comment.objects.bulk_create(all_comment)
+    #return all_comment
+    all = {'all_comment': all_comment, 'new': new}
+    #all = ['new'] = new
+    return all
--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -5,3 +5,42 @@ from django.urls import reverse
 from pytest_django.asserts import assertRedirects
 
 from news.models import Comment, News
+
+
+
+
+def test_note_in_list_less_then_10(author_client, many_news):
+    url = reverse('news:home')
+    # Запрашиваем страницу со списком заметок:
+    response = author_client.get(url)
+    # Получаем список объектов из контекста:
+    object_list = response.context['object_list']
+    notes_count = object_list.count()
+    # Проверяем, что заметка находится в этом списке:
+    assert notes_count <= 10
+
+
+def test_news_order(many_news, author_client):
+    url = reverse('news:home')
+    response = author_client.get(url)
+    object_list = response.context['object_list']
+    # Получаем даты новостей в том порядке, как они выведены на странице.
+    all_dates = [news.date for news in object_list]
+    # Сортируем полученный список по убыванию.
+    sorted_dates = sorted(all_dates, reverse=True)
+    # Проверяем, что исходный список был отсортирован правильно.
+    assert all_dates == sorted_dates
+
+
+def test_comment_order(many_comments, author_client):
+    a=1
+    url = reverse('news:detail', args=(many_comments['new'].pk,))
+    response = author_client.get(url)
+    object_list = response.context['object_list']
+    a=1
+    # Получаем даты новостей в том порядке, как они выведены на странице.
+    #all_dates = [comment.date for comment in object_list]
+    # Сортируем полученный список по убыванию.
+    #sorted_dates = sorted(all_dates, reverse=True)
+    # Проверяем, что исходный список был отсортирован правильно.
+    assert 1 == 1
